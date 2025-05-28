18f05d3f0b4eaa3b2aaea929e1fd973479331158
alexcranedesigner@gmail.com <alexcranedesigner@gmail.com>
2025-05-27 22:44:33 -0700

    About background page only stripped down version of site

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
   import { RouterView } from 'vue-router';
-  import MainMenuBar from '@/components/navigation/MainMenuBar.vue'
+  // import MainMenuBar from '@/components/navigation/MainMenuBar.vue'
   import Footer from '@/components/global/GlobalFooter.vue';
 
 </script>
 
 <template>
   <div>
-    <main-menu-bar />
+    <!--<main-menu-bar />-->
     <RouterView />
     <Footer />
   </div>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import landingBanner from '@/components/landing/LandingBanner.vue'
 import MainMenuHoverImages from '@/components/navigation/MainMenuHoverImages.vue'
 </script>
@@ -10,6 +10,16 @@ import MainMenuHoverImages from '@/components/navigation/MainMenuHoverImages.vue
 
       <MainMenuHoverImages :dark-theme="true"/>
     </div>
+  </div>
+</template>-->
+
+<script setup>
+import BackgroundSummary from '@/components/background/BackgroundSummary.vue'
+</script>
+
+<template>
+  <div class="bg-[#F2C4B3] text-black p-8 pt-24">
+    <background-summary />
   </div>
 </template>
 
