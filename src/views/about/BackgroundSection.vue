29e5944ac1048554f5a0479a10eb9b7b896525a8
alexcranedesigner@gmail.com <alexcranedesigner@gmail.com>
2025-06-10 17:25:55 -0700

    added images to background page

--- src/views/about/BackgroundSection.vue
+++ src/views/about/BackgroundSection.vue
@@ -1,9 +1,88 @@
 <script setup>
 import BackgroundSummary from '@/components/background/BackgroundSummary.vue'
+import backgroundAbstract1 from '@/assets/images/background-abstract-1.png'
+import backgroundAbstract2 from '@/assets/images/background-abstract-2.png'
 </script>
 
 <template>
   <div class="bg-[#F2C4B3] text-black p-8 pt-24">
     <background-summary />
+
+
+    <div class="bg-[#F2C4B3] p-5 text-[#1c1c1c] max-w-screen w-full">
+      <div class="max-w-[15ch] mx-auto my-6">
+        <p class="mb-4">
+          Yeah... it's been a long road and the end isn't in sight yet. The quest for realness in this world is slow and intense. I wouldn't say that being a small cog in a large wheel is my style. As a result, I've rarely been strictly focused on one professional discipline. Much of the noteworthy work I’ve done has been on a freelance or informal basis. Here are some highlights:
+        </p>
+      </div>
+
+      <div class="w-full h-auto">
+        <img :src="backgroundAbstract1" alt="Garage music" width="full" class="w-full h-auto object-contain"/>
+      </div>
+
+      <ul class="space-y-6 list-disc pl-5">
+        <li>
+          I engineered in-house at the legendary San Francisco recording studio Hyde Street Studios and worked as a live sound engineer at Club Congress in Tucson’s Congress Hotel.
+          The artists I’ve worked with have included
+          <a href="https://en.wikipedia.org/wiki/George_Clinton_(funk_musician)" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">George Clinton and the P-Funk All Stars</a>,
+          <a href="https://en.wikipedia.org/wiki/Hieroglyphics_(group)" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Hieroglyphics</a>,
+          <a href="https://en.wikipedia.org/wiki/Ziggy_Modeliste" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Zigaboo Modeliste</a>, and
+          <a href="https://en.wikipedia.org/wiki/Sage_Francis" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Sage Francis</a>.
+        </li>
+        <li>
+          Having founded or joined over a dozen underground bands, I’ve played concerts with a lot of different artists, including some acts I really like:
+          <a href="https://en.wikipedia.org/wiki/Suicidal_Tendencies" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Suicidal Tendencies</a>,
+          <a href="https://en.wikipedia.org/wiki/Qui_(band)" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Qui (with David Yow on vocals)</a>,
+          <a href="https://en.wikipedia.org/wiki/Dwarves_(band)" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Dwarves (with Nick Oliveri on bass)</a>,
+          <a href="https://en.wikipedia.org/wiki/Author_&amp;_Punisher" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Author and Punisher</a>,
+          <a href="https://www.instagram.com/yearofthecobra/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Year of the Cobra</a>,
+          <a href="https://www.instagram.com/mammothgrinder/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Mammoth Grinder</a>, and
+          <a href="https://www.instagram.com/electrichawkband/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Electric Hawk</a>.
+        </li>
+        <li>
+          I’ve Studied music with tabla guru
+          <a href="https://alokedutta.com/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Aloke Dutta</a>, drumset master
+          <a href="https://daveelitch.com/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Dave Elitch</a>, guitar expert
+          <a href="https://patrickworley.com/about/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Patrick Worley</a>, and music coach extraordinaire
+          <a href="http://www.treygunn.com/bio/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Trey Gunn</a>.
+        </li>
+        <li>
+          I won a contest hosted by Indaba Music for
+          <a href="https://soundcloud.com/dissonantdreamland/valley-of-smoke-tca-remix" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">remixing</a> an
+          <a href="https://en.wikipedia.org/wiki/Intronaut" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Intronaut</a> song.
+        </li>
+        <li>
+          Former
+          <a href="https://en.wikipedia.org/wiki/American_Institute_of_Graphic_Arts" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">AIGA (American Institute of Graphic Artists)</a> member and contributor.
+        </li>
+        <li>
+          I spent years as part of a team designing technical equipment at a biotech startup called
+          <a href="https://sofie.com/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Sofie Biosciences</a>.
+        </li>
+        <li>
+          I used my design and tech skills to help set the wheels in motion for the artist-friendly music streaming alternative
+          <a href="https://resonate.coop/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Resonate</a>.
+        </li>
+        <li>
+          I collaborated with an engineer friend to create the
+          <a href="https://alexcrane.xyz/kinetic-beats/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Kinetic Beats</a> air drumming iPhone app.
+        </li>
+        <li>
+          I was hired to build websites for some musicians I admire
+          <a href="https://daveelitch.com/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Dave Elitch</a>,
+          <a href="https://www.trevordunn.net/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Trevor Dunn</a>,
+          Dunable Guitars, and
+          <a href="https://alokedutta.com/" target="_blank" class="text-[#9A2C2C] underline hover:opacity-80">Aloke Dutta</a>.
+        </li>
+      </ul>
+
+      <div class="w-full h-auto">
+        <img :src="backgroundAbstract2" alt="Garage music" width="full" class="w-full h-auto object-contain"/>
+      </div>
+
+      <p class="mt-4 mb-4">
+        At some point when I was relatively young, before I knew what the term even meant, I became painfully aware of the concept of ideology. One of the most complicated aspects of life is navigating through a sea of different ideologies. A multitude of worldviews and agendas is at once what makes the world so interesting, but also so frustrating. I was born in upstate New York and lived in the mountains of Massachusetts until I was 13, when my family moved to a rural part of Arizona near the Mexican border. As an adult, I’ve mostly lived in larger cities – San Francisco, Tucson, Los Angeles, Las Vegas, and Eugene. I’ve also traveled and worked internationally. One of the great pleasures in my life has been knowing and working with a vast array of different humans. One of the great complexities has been trying to fathom the often perplexing and contradictory belief systems that people hold. This is especially complicated considering that I personally avoid pre-ordained world views. I think this long running curiosity about ideology is why the life of an artist suits me.
+      </p>
+    </div>
   </div>
 </template>
