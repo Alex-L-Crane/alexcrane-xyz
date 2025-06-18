109966b301da8692da66d4748b025efb4dafae4a
alexcranedesigner@gmail.com <alexcranedesigner@gmail.com>
2025-06-17 22:12:52 -0700

    sticky images on background section

--- src/views/about/BackgroundSection.vue
+++ src/views/about/BackgroundSection.vue
@@ -7,7 +7,7 @@ import fieldAmp from '@/assets/images/alex-field-amp-cropped-1.png'
 </script>
 
 <template>
-  <div class="bg-[#F2C4B3] text-black pt-24">
+  <div class="bg-[#F6D9CE] text-[#444244] pt-24">
     <div class="px-8 pb-16">
       <background-summary />
     </div>
@@ -44,11 +44,11 @@ import fieldAmp from '@/assets/images/alex-field-amp-cropped-1.png'
       </div>
     </div>
 
-    <div class="-mt-36 bg-[#5D564B] p-16 mr-16 mb-32 mx-8">
-      <h2 class="neogeo text-[#F6E5D2] text-7xl tracking-tight mb-3">It's far better to quietly baffle and annoy than to be tame.</h2>
+    <div class="-mt-36 bg-[#c6ccd7] rounded p-16 mr-16 mb-32 mx-8">
+      <h2 class="neogeo text-[#3E3D3F] text-7xl tracking-tight mb-3">It's far better to quietly baffle and annoy than to pacify.</h2>
     </div>
 
-    <div class="w-full mb-24 flex p-8">
+    <div class="w-full flex p-8">
       <div class="w-[50%]">
         <div class="max-w-[15ch]">
           <h2 class="neogeo text-5xl/[1] mb-3">The pursuit of sound.</h2>
@@ -115,31 +115,37 @@ import fieldAmp from '@/assets/images/alex-field-amp-cropped-1.png'
     </div>
 
     <div class="max-w-5xl px-8">
-      <img :src="backgroundAbstract2" alt="Garage music" width="full" class=" mb-8"/>
+      <img :src="backgroundAbstract2" alt="Garage music" width="full"/>
     </div>
 
-    <div class="flex flex-row w-full p-8">
-      <div class="mt-96">
-        <img :src="fieldAmp" alt="Sitting in a field adjusting an amplifier" width="full"/>
+    <div class="w-full mb-24 flex p-8">
+      <div class="w-[50%]">
+        <div class="relative h-full">
+          <div class="sticky top-32 z-10">
+            <img :src="fieldAmp" alt="Sitting in a field adjusting an amplifier" width="full"/>
+          </div>
+        </div>
       </div>
-      <div class="max-w-[15ch] pl-16 mb-6 ">
-        <p>
-          I didn’t know what the word “ideology” meant when I first felt it — but I knew it was everywhere.
+      <div class="w-[50%]">
+        <div class="max-w-[15ch] pl-16 mb-96">
+          <p>
+            I didn’t know what the word “ideology” meant when I first felt it — but I knew it was everywhere.
 
-          Trying to make sense of the world meant swimming through a mess of beliefs, each one louder than the last. It’s what makes life fascinating. And exhausting.
+            Trying to make sense of the world meant swimming through a mess of beliefs, each one louder than the last. It’s what makes life fascinating. And exhausting.
 
-          I was born in upstate New York, spent my childhood in the mountains of western Massachusetts, then got dropped into rural Arizona at thirteen — desert, border towns, a different rhythm entirely.
+            I was born in upstate New York, spent my childhood in the mountains of western Massachusetts, then got dropped into rural Arizona at thirteen — desert, border towns, a different rhythm entirely.
 
-          Since then: San Francisco. Tucson. L.A. Vegas. Eugene. Planes. Passports.
+            Since then: San Francisco. Tucson. L.A. Vegas. Eugene. Planes. Passports.
 
-          I’ve worked with people from just about every walk of life. That’s been one of the great joys.
+            I’ve worked with people from just about every walk of life. That’s been one of the great joys.
 
-          But what they believe? That’s the puzzle.
+            But what they believe? That’s the puzzle.
 
-          I’ve never done well with ready-made worldviews. I’m not wired for doctrine.
+            I’ve never done well with ready-made worldviews. I’m not wired for doctrine.
 
-          Maybe that’s why I ended up here — chasing meaning the only way I know how: through art, through work, through rhythm.
-        </p>
+            Maybe that’s why I ended up here — chasing meaning the only way I know how: through art, through work, through rhythm.
+          </p>
+        </div>
       </div>
     </div>
   </div>
