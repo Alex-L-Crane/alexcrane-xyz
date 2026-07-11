02eeb718388e0579fc92d91f5e86a756dc6e1f96
Alex Crane <alexcrane-m2@Alexs-MacBook-Pro.local>
2026-07-11 00:30:56 -0700

    Remove dead commented-out code from LandingPage.vue
    Leftover experimental markup (scramble text/banner, an unused
    BackgroundSection variant) that was never active.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -7,27 +7,7 @@ import ProjectstFeed from '@/views/ProjectsFeed.vue'
     <section>
       <ProjectstFeed />
     </section>
-    <!--<div class="h-screen flex flex-col px-8 pt-8">
-      <div class="flex-grow flex items-center justify-center">
-        <scramble-text />
-      </div>
-      <img :src="landingBanner" alt="Equipment in a garage studio" width="auto" height="auto" class="w-full"/>
-    </div>
-
-    <div class="pt-4 px-8 pb-8 max-w-5xl mx-auto">
-      <main-menu :light-theme="true"/>
-    </div>-->
   </div>
 </template>
 
-<!-- <script setup>
-import BackgroundSection from '@/views/about/BackgroundSection.vue'
-</script>
-
-<template> -->
-  <!--<div class="bg-[#F2C4B3] text-black p-8 pt-24">-->
-  <!-- <div class="bg-[#F2C4B3] text-black">
-    <background-section />
-  </div>
-</template>-->
 
