ae7a8ddf501966a6d133b165d366c6cfd10a61ad
Alex Crane <alexcrane-m2@Alexs-MacBook-Pro.local>
2026-08-14 07:32:44 -0700

    Tech feed: added one man rig, guitar reamping, and little bits posts

--- src/content/videos.js
+++ src/content/videos.js
@@ -4,6 +4,15 @@ import preparedDrumsThumbSrcset from '@/assets/images/feed/prepared-drums.png?w=
 import vivaCascadiaThumb from '@/assets/images/feed/viva-cascadia-playthrough.png?w=800&format=webp&quality=82'
 import vivaCascadiaThumbSrcset from '@/assets/images/feed/viva-cascadia-playthrough.png?w=480;800;1024&format=webp&quality=82&as=srcset'
 
+import oneManRigRundownThumb from '@/assets/images/feed/one-man-rig-rundown.png?w=800&format=webp&quality=82'
+import oneManRigRundownThumbSrcset from '@/assets/images/feed/one-man-rig-rundown.png?w=480;800;1024&format=webp&quality=82&as=srcset'
+
+import littleBitsThumb from '@/assets/images/feed/little-bits.png?w=800&format=webp&quality=82'
+import littleBitsThumbSrcset from '@/assets/images/feed/little-bits.png?w=480;800;1024&format=webp&quality=82&as=srcset'
+
+import guitarReampingThumb from '@/assets/images/feed/guitar-reamping.png?w=800&format=webp&quality=82'
+import guitarReampingThumbSrcset from '@/assets/images/feed/guitar-reamping.png?w=480;800;1024&format=webp&quality=82&as=srcset'
+
 
 export const drumVideos = [
   {
@@ -18,7 +27,7 @@ export const drumVideos = [
   },
   {
     slug: 'viva-cascadia',
-    title: 'Viva Cascadia Song Playthrough',
+    title: 'Viva Cascadia Playthrough',
     date: '01-08-2025',
     youtubeId: 'TyKsowvmngs',
     description: 'Plathrough of the drum parts I wrote for the song Viva Cascadia by Eugene band Red Cloud.',
@@ -31,10 +40,32 @@ export const drumVideos = [
 export const techVideos = [
   {
     slug: 'one-man-rig-rundown',
-    title: 'One Man Rig Rundown',
+    title: 'One-man Rig Rundown',
     date: '06-27-2023',
     youtubeId: 'MrfHY10c9tU',
     description: 'An overly complicated version of a technical setup for one-person performance.',
+    thumbnail: oneManRigRundownThumb,
+    thumbnailSrcset: oneManRigRundownThumbSrcset,
+    body: ``,
+  },
+  {
+    slug: 'little-bits',
+    title: 'Little Bits',
+    date: '07-13-2015',
+    youtubeId: 'byM-ahwx-y0',
+    description: 'Messing around with a toy synth.',
+    thumbnail: littleBitsThumb,
+    thumbnailSrcset: littleBitsThumbSrcset,
+    body: ``,
+  },
+  {
+    slug: 'guitar-reamping',
+    title: 'Guitar reamping',
+    date: '03-15-2012',
+    youtubeId: 'L9pwYj0oR4E',
+    description: 'Reamping guitars for TCA album White Light and the Empire Collapse.',
+    thumbnail: guitarReampingThumb,
+    thumbnailSrcset: guitarReampingThumbSrcset,
     body: ``,
   }
 ].sort((a, b) => new Date(b.date) - new Date(a.date))
